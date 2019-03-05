340b8da240de82e5cae78610620dd12a31fe3137
Soukaina <erraji01@gmail.com>
2019-03-05 20:11:55 +0100

    setup webSite

---
+++ script1.js
@@ -0,0 +1,70 @@
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+var pos = 0;
+var turn = 0;
+var data = ['Chef the projet multimedia','Designer','développer Front-End'];
+var speed = 200;
+
+setTimeout(typeWriter, speed);
+
+function typeWriter() {
+  if (pos < data[turn].length) {
+    document.getElementById("demo").innerHTML += data[turn].charAt(pos);
+    console.log(data[turn].charAt(pos));
+    pos++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(erase, speed+100);
+  }
+}
+
+function erase() {
+  if (pos >= 0) {
+      var str=data[turn].toString().substring(0, pos);
+      document.getElementById("demo").innerHTML = str;
+      pos--;
+      setTimeout(erase, speed-100);
+    } else {
+      turn++;
+      if(turn>=data.length) 
+        turn=0;
+      setTimeout(typeWriter, speed);
+    }
+}
